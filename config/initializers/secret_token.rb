b13ea5e60ba959ce425945cbe633d1afe55c6f74
Manuel Schiner <manuel.schiner@ivoke.de>
2012-07-13 14:02:33 +0200

    Generated rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuMongo::Application.config.secret_token = '4a5dd04e736806f7cb6ef08195af9b0116ce2105775a133e9db152f8288ae07d25776be3e10a3ae03837fac40df710e157d9d51ae2ea2e66e78b4e19a609cefa'
